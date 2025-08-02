65f7121c47d7344c0e42e73fa033f064a1fb48c9
raju116433 <tanvirraju111@gmail.com>
2025-08-02 23:47:54 +0600

    Update array2.c

--- array2.c
+++ array2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int array1[7] = {10, 12, 15, 16, 18, 20, 27};
+    int array2[7];
+    int i;
+
+    // Copying array1 to array2
+    for (i = 0; i < 7; i++) {
+        array2[i] = array1[i];
+    }
+
+    // Printing array2
+    printf("array2: ");
+    for (i = 0; i < 7; i++) {
+        printf("%d ", array2[i]);
+    }
+
+    return 0;
+}
+
 
